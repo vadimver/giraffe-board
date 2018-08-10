f35a2bc648e1c10ab1be93cc3449e85cb441539b
vadimver <vadimvereikin@gmail.com>
2018-08-10 11:16:37 +0300

    fix index.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,17 +8,17 @@
             <div class="card">
                 <div class="card-header">
                     {{$board->title}}
-                    @if(Auth::user()->id == $board->user_id)
-                        <form action="{{ url("/edit/$board->id") }}" method="POST">
-                            <input type="hidden" name="_method" value="delete" />
-                            {{ csrf_field() }}
-                            <button type="submit" class="btn btn-warning btn-edit"><i class="fas fa-edit"></i></button>
-                        </form>
-                        <form action="{{ url("/delete/$board->id") }}" method="POST">
-                            <input type="hidden" name="_method" value="delete" />
-                            {{ csrf_field() }}
-                            <button type="submit" class="btn btn-danger btn-del">x</button>
-                        </form>
+                    @if (isset(Auth::user()->id))
+                        @if(Auth::user()->id == $board->user_id)
+                            <form action="{{ url("/edit/$board->id") }}" method="GET">
+                                <button type="submit" class="btn btn-warning btn-edit"><i class="fas fa-edit"></i></button>
+                            </form>
+                            <form action="{{ url("/delete/$board->id") }}" method="POST">
+                                <input type="hidden" name="_method" value="delete" />
+                                {{ csrf_field() }}
+                                <button type="submit" class="btn btn-danger btn-del">x</button>
+                            </form>
+                        @endif
                     @endif
                 </div>
                 <div class="card-body">
